0cc23b18ee7192d3cfb609464d16bbfafc92477e
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-06-20 13:41:07 +0200

    Add Exo 1

---
+++ Exercice_1/main.js
@@ -0,0 +1,2 @@
+document.querySelector('#image1').addEventListener('mouseenter', (e) => e.target.src = "images/image1_2.jpg");
+document.querySelector('#image1').addEventListener('mouseleave', (e) => e.target.src = "images/image1.jpg");
